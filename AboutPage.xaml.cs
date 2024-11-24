aebb454b3c53883486c50186d96f430516844cf2
GABRIELA-MIHAELA LIONTE <gabriela.lionte@stud.ubbcluj.ro>
2024-11-24 15:20:27 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace LionteGabrielaLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace LionteGabrielaLab7
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
